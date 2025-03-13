b0513b01266df3ee69252e81cb25cc88813ce209
Ziad Gad <ziadamrjad@gmail.com>
2025-03-13 20:01:21 +0200

    root welcome message

--- app.js
+++ app.js
@@ -36,28 +36,49 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(helmet());
 
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP. Please try again in an hour!',
+const apiLimiter = rateLimit({
+  max: 300,
+  windowMs: 15 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again after 15 minutes!',
 });
-app.use('/api', limiter);
+
+const loginLimiter = rateLimit({
+  max: 20,
+  windowMs: 15 * 60 * 1000,
+  message:
+    'Too many login attempts from this IP, please try again after 15 minutes!',
+});
+
+app.use('/api', apiLimiter);
+app.use('/api/v1/users/login', loginLimiter);
 
 app.use(mongoSanitize());
 
 app.use(xss());
 
-app.use(hpp());
-
+app.use(
+  hpp({
+    whitelist: [
+      'price',
+      'quantity',
+      'sold',
+      'ratingsAverage',
+      'ratingsQuantity',
+    ],
+  }),
+);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 // Body Parser Middlewares
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '20kb' }));
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to TryOn API' });
+});
 app.use('/api/v1/categories', categoryRouter);
 app.use('/api/v1/subcategories', subCategoryRouter);
 app.use('/api/v1/products', productRouter);
